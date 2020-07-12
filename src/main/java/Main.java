c93f44247152634817fcc84e4c7cff96bbf175ae
NahrebetskyiPavel <pnagrebetskyi@gmail.com>
2020-07-12 17:51:47 +0300

    refactor

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        ConsoleInput in = new ConsoleInput();
+        Pyth calc = new Pyth();
+        calc.iSNaturalNum(in.intInput());
+    }
+}
---
+++ src/main/java/Pyth.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pyth {
+    private static List<int[]> values = new ArrayList<>();
+
+    public void iSNaturalNum(int n) {
+
+        if (n > 0) {
+            findPossibleValues(n);
+            printValues();
+        } else {
+            System.out.println("Please chose natural number");
+        }
+
+    }
+
+    private void findPossibleValues(int n) {
+        int a;
+        int b;
+        int c;
+        for (c = 0; c <= n; c++) {
+            for (b = 0; b <= c; b++) {
+                for (a = 0; a <= b; a++) {
+                    if (a * a + b * b == c * c) {
+                        values.add(new int[]{a, b, c});
+                    }
+                }
+            }
+        }
+
+
+    }
+
+    private void printValues() {
+        for (int[] value : values) {
+            System.out.printf("a = %d \t", value[0]);
+            System.out.printf("b = %d \t", value[1]);
+            System.out.printf("c = %d \n", value[2]);
+        }
+        if (values.isEmpty()) System.out.println("No matches");
+    }
+}
