c07a7edcd49073ea3db67024083fd127e43cdc33
bebebe <hahaha@gmail.com>
2020-08-10 16:57:36 +0300

    Develop Language Component

--- src/test/java/com/softserve/edu/greencity/ui/pages/habits/MyHabitsPage.java
+++ src/test/java/com/softserve/edu/greencity/ui/pages/habits/MyHabitsPage.java
@@ -2,6 +2,8 @@ package com.softserve.edu.greencity.ui.pages.habits;
 
 import org.openqa.selenium.WebDriver;
 
+import com.softserve.edu.greencity.ui.data.Languages;
+import com.softserve.edu.greencity.ui.pages.about.AboutPage;
 import com.softserve.edu.greencity.ui.pages.common.TopPart;
 
 public class MyHabitsPage extends TopPart {
@@ -18,5 +20,12 @@ public class MyHabitsPage extends TopPart {
     // Page Object
 
     // Functional
+
+    // Business Logic
+
+    public MyHabitsPage switchLanguage(Languages language) {
+        chooseLanguage(language);
+        return new MyHabitsPage(driver);
+    }
 
 }
