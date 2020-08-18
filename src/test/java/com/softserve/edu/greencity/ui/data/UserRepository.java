4c81e55a813b40b8e5c65c6d8f2c8c77c1b296a7
NahrebetskyiPavel <pnagrebetskyi@gmail.com>
2020-08-18 11:20:59 +0300

    AllureLifecycle OFF

--- src/test/java/com/softserve/edu/greencity/ui/data/UserRepository.java
+++ src/test/java/com/softserve/edu/greencity/ui/data/UserRepository.java
@@ -1,6 +1,7 @@
 package com.softserve.edu.greencity.ui.data;
 
 import com.softserve.edu.greencity.ui.tools.Randomizer;
+import io.qameta.allure.Step;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -32,12 +33,13 @@ public final class UserRepository {
     }
 
     //replace System.getenv().get(property.getProperty("temporaryPass"))); to property.getProperty("temporaryPass"));
+    @Step("get temporary credentials properties")
     public User temporary() {
         return new User(
                 property.getProperty("temporaryLoginName"),
                 property.getProperty("temporaryPass"));
     }
-
+    @Step("get default User credentials properties")
     public User defaultUserCredentials() {
         return new User(
                 property.getProperty("defaultName"),
@@ -45,15 +47,15 @@ public final class UserRepository {
                 property.getProperty("defaultPass"),
                 property.getProperty("defaultPass"));
     }
-
+    @Step("get google User  credentials properties")
     public User googleUserCredentials() {
         return new User(property.getProperty("googleEmail"), property.getProperty("googlePass"));
     }
-
+    @Step("get empty User credentials properties")
     public User emptyUserCredentials() {
         return new User("", "", "", "");
     }
-
+    @Step("get invalid Usercredentials properties")
     public User invalidUserCredentials() {
         return new User(
                 property.getProperty("invalidName"),
@@ -61,7 +63,7 @@ public final class UserRepository {
                 property.getProperty("invalidPass"),
                 property.getProperty("invalidPass"));
     }
-
+    @Step("get invalid Name credentials properties")
     public User invalidNameCredentials() {
         return new User(
                 property.getProperty("invalidName"),
@@ -69,7 +71,7 @@ public final class UserRepository {
                 property.getProperty("defaultPass"),
                 property.getProperty("defaultPass"));
     }
-
+    @Step("get unregister User credentials properties")
     public User unregisterUser() {
         return new User(
                 property.getProperty("defaultName"),
@@ -77,7 +79,7 @@ public final class UserRepository {
                 property.getProperty("temporaryPass"),
                 property.getProperty("comfTemporaryPass"));
     }
-
+    @Step("get   invalid Email User Credentials")
     public User invalidEmailUserCredentials() {
         return new User(
                 property.getProperty("defaultName"),
@@ -85,7 +87,7 @@ public final class UserRepository {
                 property.getProperty("defaultPass"),
                 property.getProperty("defaultPass"));
     }
-
+    @Step("get invalid Confirm Pass Credentials")
     public User invalidConfirmPassCredentials() {
         return new User(
                 property.getProperty("defaultName"),
@@ -93,21 +95,21 @@ public final class UserRepository {
                 property.getProperty("defaultPass") + "!",
                 property.getProperty("defaultPass"));
     }
-
+    @Step("get user With Empty Email Field credentials properties")
     public User userWithEmptyEmailField() {
         return new User("", property.getProperty("temporaryPass"));
     }
-
+    @Step("get   user With Empty Password Field credentials properties")
     public User userWithEmptyPasswordField() {
         return new User(property.getProperty("temporaryLoginName"), "");
     }
-
+    @Step("get user Credentials With Invalid Password ")
     public User userCredentialsWithInvalidPassword() {
         return new User(
                 property.getProperty("temporaryLoginName"),
                 property.getProperty("validIncorrectPassword"));
     }
-
+    @Step("get user Credentials For Registration")
     public User userCredentialsForRegistration() {
         return new User(
                 property.getProperty("nameForRegistration"),
@@ -115,7 +117,7 @@ public final class UserRepository {
                 property.getProperty("passwordForRegistration"),
                 property.getProperty("passwordForRegistration"));
     }
-
+    @Step("get invalid Pass Uppercase User Creds credentials properties")
     public User invalidPassUppercaseUserCreds() {
         return new User(
                 property.getProperty("defaultName"),
@@ -123,7 +125,7 @@ public final class UserRepository {
                 property.getProperty("invalidPassUppercase"),
                 property.getProperty("invalidPassUppercase"));
     }
-
+    @Step("get invalid Pass Digit User credentials properties")
     public User invalidPassDigitUserCreds() {
         return new User(
                 property.getProperty("defaultName"),
@@ -131,7 +133,7 @@ public final class UserRepository {
                 property.getProperty("invalidPassDigit"),
                 property.getProperty("invalidPassDigit"));
     }
-
+    @Step("get invalid Pass Lowercase User credentials properties")
     public User invalidPassLowercaseUserCreds() {
         return new User(
                 property.getProperty("defaultName"),
@@ -139,7 +141,7 @@ public final class UserRepository {
                 property.getProperty("invalidPassLowercase"),
                 property.getProperty("invalidPassLowercase"));
     }
-
+    @Step("get invalid Pass Spec Char User credentials properties")
     public User invalidPassSpecCharUserCreds() {
         return new User(
                 property.getProperty("defaultName"),
@@ -147,7 +149,7 @@ public final class UserRepository {
                 property.getProperty("invalidPassSpecChar"),
                 property.getProperty("invalidPassSpecChar"));
     }
-
+    @Step("get invalid Pass Length User credentials properties")
     public User invalidPassLengthUserCreds() {
         return new User(
                 property.getProperty("defaultName"),
@@ -155,7 +157,7 @@ public final class UserRepository {
                 property.getProperty("invalidPassLength"),
                 property.getProperty("invalidPassLength"));
     }
-
+    @Step("get invalid Pass Space User credentials properties")
     public User invalidPassSpaceUserCreds() {
         return new User(
                 property.getProperty("defaultName"),
@@ -163,7 +165,7 @@ public final class UserRepository {
                 property.getProperty("invalidPassSpace"),
                 property.getProperty("invalidPassSpace"));
     }
-
+    @Step("get   random number")
     private String getRandom() {
         return String.format("%s, %d", "+", (int) (Math.random() * ((Integer.MAX_VALUE - 1) - 10 + 1) + 1))
                 .replaceAll("\\s+", "")
