68583e3dff568e1a421ab7183b9f8c80421991da
NahrebetskyiPavel <pnagrebetskyi@gmail.com>
2020-08-18 15:55:36 +0300

    add convenient way to search Web Elements

---
+++ src/test/java/com/softserve/edu/greencity/ui/tools/StableWebElementSearch.java
@@ -0,0 +1,40 @@
+package com.softserve.edu.greencity.ui.tools;
+
+import com.google.common.base.Function;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+import lombok.SneakyThrows;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Parameters;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * convenient way to search Web Elements
+ */
+public interface StableWebElementSearch {
+    WebDriver setDriver();
+    @Description("Find element By CSS")
+    default public WebElement $(By locator){
+        return assertThat(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    @Step
+    @Description("Find element By xpath")
+    default public WebElement $x(String xpath){
+        return assertThat(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+    }
+
+    @Step
+    @Description("short explicit wait visibility Of element")
+        default public <V> V assertThat(Function<? super WebDriver, V> condition){
+        setDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        V element = (new WebDriverWait(setDriver(),5)).until(condition);
+        setDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        return element;
+    }
+}
