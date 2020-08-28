dec7c0c1ae135084e20ad5522ec9ab5722eed807
bebebe <hahaha@gmail.com>
2020-08-28 10:24:48 +0300

    Maven Project Template

---
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -0,0 +1,20 @@
+package com.softserve.edu;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest {
+    
+    @Test
+    public void checkApp1() {
+        Assert.assertTrue(true);
+    }
+    
+    @Test
+    public void checkApp2() {
+        System.out.println("***surefire.application.password = " + System.getProperty("surefire.application.password"));
+        System.out.println("***System.getenv().MY_PASSWORD = " + System.getenv().get("MY_PASSWORD"));
+        System.out.println("***System.getenv().MY_IDE = " + System.getenv().get("MY_IDE"));
+        Assert.assertTrue(true);
+    }
+}
